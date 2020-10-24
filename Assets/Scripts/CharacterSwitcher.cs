2d59fd3b9b2a7a6d91e75f9d009383050880db6b
7max18 <maxkoppel0703@gmail.com>
2020-10-24 17:31:59 -0400

    Added Character switching

---
+++ Assets/Scripts/CharacterSwitcher.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class CharacterSwitcher : MonoBehaviour
+{
+    public GameObject[] characters = new GameObject[3];
+    // Start is called before the first frame update
+    void Start()
+    {
+        characters[0] = GameObject.Find("Zombie");
+        characters[1] = GameObject.Find("Skeleton");
+        characters[2] = GameObject.Find("Ghost");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ToggleActiveChar(0);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ToggleActiveChar(1);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ToggleActiveChar(2);
+        }
+    }
+
+    void ToggleActiveChar(int i)
+    {
+        UnityEngine.Debug.Log(i);
+
+        foreach(GameObject obj in characters)
+        {
+            if(System.Array.IndexOf(characters, obj) == i)
+            {
+                obj.GetComponent<PlayerMovement>().enabled = true;
+                obj.transform.GetChild(0).gameObject.SetActive(true);
+            }
+            else
+            {
+                obj.GetComponent<PlayerMovement>().enabled = false;
+                obj.transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   public float moveSpeed = 5f;
-   public Rigidbody2D rb;
-   Vector2 movement;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //if(Input.GetKeyDown)
     }
 
     void FixedUpdate(){
