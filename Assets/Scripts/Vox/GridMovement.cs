b8d0624b42dd6222dff2f98f3bcfc784ed21e077
vphoton <keengman.2018@mymail.becker.edu>
2020-10-25 02:42:22 -0400

    added grid movement
    still need to add animation switching but direction changing and movement stuff is all set up.

---
+++ Assets/Scripts/Vox/GridMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovement : MonoBehaviour
+{
+    [SerializeField] int gridSize = 1;
+    [SerializeField] float moveDuration = 1.0f;
+    float timeElapsed = 0f;
+    float normalizedTime = 0f;
+    [SerializeField] int gridX = 0;
+    [SerializeField] int gridY = 0;
+    [SerializeField] float playerScale = 0.5f;
+    int gridXlast = 0;
+    int gridYlast = 0;
+    float slideX = 0;
+    float slideY = 0;
+    int hMov = 0;
+    int vMov = 0;
+    bool moving = false;
+    void Start(){
+        gridX = (int)Mathf.Round(transform.position.x/(float)gridSize);
+        gridY = (int)Mathf.Round(transform.position.y/(float)gridSize);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //change grid position
+        if (!moving){
+            hMov = 0;
+            vMov = 0;
+            gridXlast = gridX;
+            gridYlast = gridY;
+            if (Input.GetKeyDown(KeyCode.D)){
+                gridX++;
+                hMov = 1;
+                moving = true;
+                transform.localScale = new Vector3(-playerScale,playerScale,playerScale);
+            }
+            if (Input.GetKeyDown(KeyCode.A)){
+                gridX--;
+                hMov = -1;
+                moving = true;
+                transform.localScale = new Vector3(playerScale,playerScale,playerScale);
+            }
+            if (Input.GetKeyDown(KeyCode.S)){
+                gridY--;
+                vMov = -1;
+                moving = true;
+            }
+            if (Input.GetKeyDown(KeyCode.W)){
+                gridY++;
+                vMov = 1;
+                moving = true;
+            }
+            timeElapsed = 0f;
+        }
+        
+        //lerp to grid position
+        if (moving){
+            if (timeElapsed>moveDuration){
+                moving = false;
+            } else {
+                timeElapsed += Time.deltaTime;
+                normalizedTime = timeElapsed / moveDuration;
+                normalizedTime = Easing.Cubic.InOut(normalizedTime);
+            }
+            slideX = Mathf.Lerp(0,gridSize*hMov,normalizedTime);
+            slideY = Mathf.Lerp(0,gridSize*vMov,normalizedTime);
+            transform.position = new Vector3(gridXlast*gridSize+slideX,gridYlast*gridSize+slideY,0);
+        }
+        slideX+=(gridX-slideX);
+    }
+}
