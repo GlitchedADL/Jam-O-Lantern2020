5b36fca4b354736d71d552f4574f383f582fd71e
vphoton <keengman.2018@mymail.becker.edu>
2020-10-28 16:05:45 -0400

    gravestone randomizer
    gravestones now can choose a random sprite on create and flipX randomly for variation

---
+++ Assets/Scripts/Vox/RandSprite.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandSprite : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] rSprite;
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = rSprite[Random.Range(0,rSprite.Length)];
+        GetComponent<SpriteRenderer>().flipX = (Random.value < 0.5f);
+    }
+
+}
