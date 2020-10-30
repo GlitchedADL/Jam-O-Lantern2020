9045e7d58f7585eb8c816c2d4b9f03adbfb6dba0
Warspiteful <AlainaTheLion@gmail.com>
2020-10-30 02:28:38 -0400

    Added Volume Bar Functionality

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public  AudioMixer audioMixer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void SetVolume(float volume){
+       audioMixer.SetFloat("volume",volume);
+   }
 }
